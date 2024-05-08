596a2c5d836b1e36b4b886a17ce9d7e766e038e5
Tove-Larsson <144339361+Tove-Larsson@users.noreply.github.com>
2024-05-08 15:29:46 +0200

    Added selenium tests

--- src/test/java/se/reky/hakan/web/PlayerControllerTest.java
+++ src/test/java/se/reky/hakan/web/PlayerControllerTest.java
@@ -1,12 +1,16 @@
 package se.reky.hakan.web;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Disabled
 class PlayerControllerTest {
@@ -20,7 +24,6 @@ class PlayerControllerTest {
 
     @BeforeEach
     public void setUp() {
-        //ersätt ev med din aktuella driver
         driver = new ChromeDriver();
     }
 
@@ -32,10 +35,35 @@ class PlayerControllerTest {
 
     }
 
-    public void test() {
-       driver.get("http://localhost:8080/players");
+
+    @Test
+    public void testAmountOfPlayers() {
+        driver.get("http://localhost:8080/players");
+        List<WebElement> amountOfPlayers = driver.findElements(By.tagName("li"));
+        Assertions.assertEquals(2, amountOfPlayers.size());
 
     }
+
+    @Test
+    public void verifyNameDisplayOnPlayer() {
+        driver.get("http://localhost:8080/players");
+        WebElement element = driver.findElement(By.tagName("li"));
+        assertTrue(element.isDisplayed());
+
+    }
+
+    @Test
+    public void testWebSiteTitle() {
+       driver.get("http://localhost:8080/players");
+        Assertions.assertEquals("Players List", driver.getTitle());
+    }
+
+    @Test
+    public void testButtonText() {
+        driver.get("http://localhost:8080/players");
+        Assertions.assertEquals("Logga in", driver.findElement(By.tagName("button")).getText());
+    }
+
 
 
 
